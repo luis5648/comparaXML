390c4299daeacd98deeca0ed034944887d3efe16
Luis Román <porta5648@gmail.com>
2019-04-01 20:43:35 -0600

    files

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.custommonkey.xmlunit.DetailedDiff;
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.Difference;
+import org.xml.sax.SAXException;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
